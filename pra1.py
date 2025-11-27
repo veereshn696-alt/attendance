c5c22f3a4bc3923b5179f132485c76429d56c0b9
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 16:34:48 +0530

    hlo

---
+++ pra1.py
@@ -0,0 +1,11 @@
+import sys
+if len(sys.argv)!=3:
+    print("usage : python script.py <class1> <class_held>")
+    no_class=10
+    class_held=7
+else :
+    script_name=sys.argv[0]
+    no_class=sys.argv[1]
+    class_held=sys.argv[2]
+per=(class_held/no_class)*100
+print("attendance is ",per)
